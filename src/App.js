08f11cd6854efcd77c4f354cca240e1815cfd8e4
Elevy <36045264+enzolevy@users.noreply.github.com>
2023-04-21 02:12:21 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+// src/App.js
+import { BrowserRouter as Router, Route, NavLink, Routes } from "react-router-dom";
+import React from 'react';
+import SimpleQuestion from "./SimpleQuestion";
+import CompatibilityTab from "./CompatibilityTab";
+import Home from './Home';
+import Logo from './logo.svg';
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div className="App-header">
+        <NavLink className="div-logo" to="/">
+          <img src={Logo} className="App-logo" alt="logo" />
+          <h1>Destin en ligne</h1>
+        </NavLink>
+      <nav className="Navigation">
+        <div className="ListNav">
+            <NavLink className="linkNav" style={({ isActive }) => ({
+                            padding: isActive ? '5px;' : '5px',
+                            margin: isActive ? '0px 2px' : '0px 2px',
+                            borderRadius : isActive ? '5px' : '5px',
+                            color: isActive ? '#fff' : '#545e6f',
+                            background: isActive ? '#6200ee' : '#f0f0f0',
+                            })} to="/">
+              Accueil
+            </NavLink>
+            <NavLink className="linkNav" style={({ isActive }) => ({
+                            padding: isActive ? '5px;' : '5px',
+                            margin: isActive ? '0px 2px' : '0px 2px',
+                            borderRadius : isActive ? '5px' : '5px',
+                            color: isActive ? '#fff' : '#545e6f',
+                            background: isActive ? '#6200ee' : '#f0f0f0',
+                            })} to="/SimpleQuestion">
+              Boule de crystale
+            </NavLink>
+            <NavLink className="linkNav" style={({ isActive }) => ({
+                            padding: isActive ? '5px;' : '5px',
+                            margin: isActive ? '0px 2px' : '0px 2px',
+                            borderRadius : isActive ? '5px' : '5px',
+                            color: isActive ? '#fff' : '#545e6f',
+                            background: isActive ? '#6200ee' : '#f0f0f0',
+                            })} to="/CompatibilityTab">
+              Compatibilité
+            </NavLink>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/CompatibilityTab" element={<CompatibilityTab />} />
+          <Route path="/SimpleQuestion" element={<SimpleQuestion />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
